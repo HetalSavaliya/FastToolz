6d72ba4aeb6d0bca0649e17d5ace66df99ec606b
HetalSavaliya <dev013.rejoice@gmail.com>
2025-08-04 12:36:59 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8822732191267343"
-     crossorigin="anonymous"></script>
+   ></script>
 
       </head>
       <body
